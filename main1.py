77c1d2269ff3e98d4c586c0ded86ed0368cd0cf9
Gentry <watsonra@iu.edu>
2019-10-07 18:12:39 -0400

    finishes assignment

--- main1.py
+++ main1.py
@@ -14,8 +14,8 @@ SCREEN_HEIGHT = 600
 MARGIN = 30
 SCREEN_TITLE = "Particle Exercise"
 
-PARTICLE_MIN_SCALE = 0.01
-PARTICLE_MAX_SCALE = 0.08
+PARTICLE_MIN_SCALE = 0.06
+PARTICLE_MAX_SCALE = 0.1
 PARTICLE_MIN_X = -20
 PARTICLE_MAX_X = 20
 PARTICLE_VELOCITY_X = 0
@@ -30,7 +30,7 @@ PARTICLE_MAX_DECAY = 0.01
 
 class Particle(arcade.Sprite):
     def __init__(self, asset, scale, x, y, dx, dy, ax, ay, decay):
-        super().__init__("assets/{}.png".format(asset), scale)
+        super().__init__("assets/fire_01.png".format(asset), scale)
         self.center_x = x
         self.center_y = y
         self.dx = dx
@@ -41,16 +41,16 @@ class Particle(arcade.Sprite):
         self.color_pos = 0
 
         self.particle_colors = [
-            (open_color.red_5, 4)
-            ,(open_color.red_4, 5)
-            ,(open_color.red_3, 6)
-            ,(open_color.red_2, 7)
-            ,(open_color.red_1, 8)
-            ,(open_color.teal_1, 8)
-            ,(open_color.teal_2, 7)
-            ,(open_color.teal_3, 6)
-            ,(open_color.teal_4, 5)
-            ,(open_color.teal_5, 4)
+            (open_color.violet_1, 4)
+            ,(open_color.violet_4, 5)
+            ,(open_color.violet_3, 6)
+            ,(open_color.violet_2, 7)
+            ,(open_color.violet_1, 8)
+            ,(open_color.pink_1, 8)
+            ,(open_color.pink_2, 7)
+            ,(open_color.pink_3, 6)
+            ,(open_color.pink_4, 5)
+            ,(open_color.pink_5, 4)
         ]
         (self.color, self.lifetime) = self.particle_colors[self.color_pos]
         self.alive = True
@@ -110,7 +110,7 @@ class Window(arcade.Window):
             decay = random.uniform(PARTICLE_MIN_DECAY,PARTICLE_MAX_DECAY)
             scale = random.uniform(PARTICLE_MIN_SCALE,PARTICLE_MAX_SCALE)
             #Particle(asset, sprite scale, initial position [x], initial position [y], velocity [x], velocity [y], acceleration [x], acceleration [y], scale decay)
-            particle = Particle('circle_05',scale,x,y,dx,dy,ax,ay,decay)
+            particle = Particle('smoke_05',scale,x,y,dx,dy,ax,ay,decay)
 
             self.particle_list.append(particle)
 
